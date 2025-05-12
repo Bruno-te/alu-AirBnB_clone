60d3da674edd72915f55745fe10ca132473da7d6
Bruno-te <i.bruno@alustudent.com>
2025-05-12 12:16:20 +0200

    alu

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,18 @@ from datetime import datetime
 class BaseModel:
     """Defines the BaseModel with id, created_at, updated_at, etc."""
 
-    def __init__(self):
-        """Initialize a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance or recreate from dictionary."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string representation of the instance."""
